b4aea855cb0d55dc697845dfc12f7bd50029ddc8
Paul Chavard <github@paul.chavard.net>
2020-07-30 11:36:31 +0200

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,18 @@ module.exports = {
     client: 'postgresql',
     connection: {
       database: 'hora_test',
+      ...(process.env.POSTGRES_HOST
+        ? { host: process.env.POSTGRES_HOST }
+        : undefined),
+      ...(process.env.POSTGRES_PORT
+        ? { port: process.env.POSTGRES_PORT }
+        : undefined),
+      ...(process.env.POSTGRES_USER
+        ? { user: process.env.POSTGRES_USER }
+        : undefined),
+      ...(process.env.POSTGRES_PASSWORD
+        ? { password: process.env.POSTGRES_PASSWORD }
+        : undefined),
     },
     pool: {
       min: 2,
@@ -23,9 +35,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'hora_production',
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
