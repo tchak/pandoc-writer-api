a5aaf78da1d2d21003eb559fe68b1ded43f06caf
Paul Chavard <github@paul.chavard.net>
2020-08-01 15:54:21 +0100

    Fix errors rendering

--- src/plugins/errors.ts
+++ src/plugins/errors.ts
@@ -1,10 +1,17 @@
 import fp from 'fastify-plugin';
 import { errorHandler } from '@koalition/error-handler';
+import { isHttpError } from 'http-errors';
 
 export default fp(async (server) => {
   server.setErrorHandler(function (error, _, reply) {
     if (error.message === 'PreconditionFailed') {
       reply.preconditionFailed();
+    } else if (isHttpError(error)) {
+      reply.status(error.statusCode);
+      reply.type('json');
+      reply.send({
+        type: error.message,
+      });
     } else {
       const { status, body } = errorHandler(error);
 
